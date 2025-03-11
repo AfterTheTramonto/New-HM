c9e7dbe15a31dffe80a791b016966e55f876223d
denis <denisik33@yandex.ru>
2025-03-11 23:03:29 +0300

    HM-check 2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 public class Book {
     private String title;
     private int publicationYear;
